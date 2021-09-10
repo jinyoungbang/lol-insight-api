2c69a4d4ed91e41b55a23286b9e9f3ccbdf26c29
Jin Young Bang <jybang@bu.edu>
2021-09-10 14:30:49 +0900

    changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.v1.routers import router
 from mangum import Mangum
 
 
 app = FastAPI(title='lol-insight-api')
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router, prefix="/v1")
 
 
