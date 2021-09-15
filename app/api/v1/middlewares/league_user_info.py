b4e2b790ed6704701b02ba62d27cfd73e7028da2
Jin Young Bang <jybang@bu.edu>
2021-09-15 21:35:05 +0900

    Fixed bug for unranked user

--- app/api/v1/middlewares/league_user_info.py
+++ app/api/v1/middlewares/league_user_info.py
@@ -16,10 +16,28 @@ def get_user_info(region: str, game_name: str):
     url = f"https://{region_router}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{game_name}?api_key={API_KEY}"
     response = requests.request("GET", url, headers={}, data={})
     response = response.json()
-    
+
     if "status" in response:
         return {
             "status": False,
             "message": f"User with the name, {game_name} does not exist."
         }
+
+    encrypted_id = response["id"]
+    url_additional = f"https://{region_router}.api.riotgames.com/lol/league/v4/entries/by-summoner/{encrypted_id}?api_key={API_KEY}"
+    response_additional = requests.request(
+        "GET", url_additional, headers={}, data={})
+    response_additional = response_additional.json()
+    if response_additional == []:
+        return response
+    response_additional = response_additional[0]
+    response["leaguePoints"] = response_additional["leaguePoints"]
+    response["wins"] = response_additional["wins"]
+    response["losses"] = response_additional["losses"]
+    response["totalGamesPlayed"] = response_additional["wins"] + \
+        response_additional["losses"]
+    response["winRate"] = round((response_additional["wins"]/(response_additional["wins"] + response_additional["losses"]))*100)
+    response["tier"] = response_additional["tier"]
+    response["rank"] = response_additional["rank"]
+
     return response
